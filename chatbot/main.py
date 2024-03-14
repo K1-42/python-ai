13453ce42e2f5a2a710fe517a2b11628e65e405c
pc_2023 <vit4242@gmail.com>
2024-03-15 00:04:46 +0900

    DBセットアップ

--- chatbot/main.py
+++ chatbot/main.py
@@ -19,12 +19,29 @@ from streamlit_chat import message
 from my_llm import llama2
 from my_llm import chatgpt
 
+# DB利用のための自作モジュール
+from my_db import llama2_db
+#from my_db import chatgpt_db
+
 # 定数定義
 LLM_LLAMA2 = 'llama2'
 LLM_CHATGPT = 'chatGPT'
 
 ##########################################################
-# イベント
+# DB登録関連のイベント
+##########################################################
+def on_db_import(llm_name, uploaded_file):
+    st.session_state["db_result"] = ''
+
+    if llm_name == LLM_LLAMA2:
+        result_message = llama2_db.import_data(uploaded_file)
+#    else:
+#        result_message = chatgpt_db.import_data(user_message)
+
+    st.session_state["db_result"] = result_message
+
+##########################################################
+# 質問関連のイベント
 ##########################################################
 
 #会話の読み込みを行う関数を定義
@@ -47,6 +64,8 @@ if "generated" not in st.session_state:
     st.session_state.generated = []
 if "past" not in st.session_state:
     st.session_state.past = []
+if 'db_result' not in st.session_state:
+  st.session_state.db_result = ''
 
 # 送信ボタンがクリックされた後の処理を行う関数を定義
 def on_input_change(llm_name, use_db):
@@ -63,8 +82,18 @@ def on_input_change(llm_name, use_db):
 ##########################################################
 # タイトルやキャプション部分のUI
 st.title("My AI Test Chatbot")
-st.caption("develop mode.")
-st.write("質問に答えます。")
+st.caption("DB登録の入力")
+with st.container():
+    uploaded_file = st.file_uploader('ファイルを選択してください。')
+    col1, col2 = st.columns(2)
+    with col1:
+        st.button("DB登録（llama2）", on_click=on_db_import, args=(LLM_LLAMA2, uploaded_file))
+    with col2:
+        st.button("DB登録（ChatGPT）", on_click=on_db_import, args=(LLM_CHATGPT, uploaded_file))
+
+st.write("DB登録結果：", st.session_state["db_result"])
+
+st.caption("質問の入力")
 
 # 会話履歴を表示するためのスペースを確保
 chat_placeholder = st.empty()
@@ -72,8 +101,8 @@ chat_placeholder = st.empty()
 # 会話履歴を表示
 with chat_placeholder.container():
     for i in range(len(st.session_state.generated)):
-        message(st.session_state.past[i],is_user=True)
-        message(st.session_state.generated[i])
+        message(st.session_state.past[i], is_user=True, key='past_'+str(i)+'_user')
+        message(st.session_state.generated[i], key='generated_'+str(i)+'_user')
 
 # 質問入力欄と送信ボタンを設置
 with st.container():
--- chatbot/my_llm/llama2.py
+++ chatbot/my_llm/llama2.py
@@ -1,4 +1,26 @@
 from llama_cpp import Llama
+from dotenv import load_dotenv
+import os
+
+# RAG構成用
+from langchain.chains import VectorDBQAWithSourcesChain
+from langchain.llms import LlamaCpp
+from langchain.vectorstores import Chroma
+from langchain.embeddings import LlamaCppEmbeddings
+from langchain.agents import load_tools
+from langchain.agents import initialize_agent
+from langchain.agents import AgentType
+from langchain.agents import Tool
+# RAG構成用（LangChainのPromptTemplateを定義）
+from langchain.chains.qa_with_sources.map_reduce_prompt import QUESTION_PROMPT
+from langchain import PromptTemplate
+
+# 設定ファイルの読み込み
+load_dotenv()
+
+# 定数定義
+LLM_PATH = os.environ["MODEL_DIR"] + os.environ["LLAMA2_MODEL_NAME"]
+DB_PATH = os.environ["DB_DIR"] + os.environ["LLAMA2_DB_NAME"]
 
 ################################
 # LLMに質問
@@ -6,7 +28,7 @@ from llama_cpp import Llama
 def query(user_message):
 
     # AIモデルの読み込み
-    llm = Llama(model_path="./my_model/llama-2-7b-chat.Q2_K.gguf")
+    llm = Llama(model_path=LLM_PATH)
 
     # 問い合わせ
     llm_return = llm(
@@ -28,5 +50,47 @@ def query(user_message):
 # LLMに質問（RAG）
 ################################
 def query_with_db(user_message):
+
+    # AIモデルの読み込み
+    llm = LlamaCpp(
+        model_path=LLM_PATH,
+        input={
+            "max_tokens": 32,
+            "stop": ["System:", "User:", "Assistant:", "\n"],
+        },
+        verbose=True,
+        n_ctx=2048,
+    )
+
+    # DBの読み込み
+    embeddings = LlamaCppEmbeddings(model_path=LLM_PATH, n_ctx=4096)
+    db = Chroma(embedding_function=embeddings, persist_directory=DB_PATH)
+    qa =VectorDBQAWithSourcesChain.from_chain_type(llm, chain_type="map_reduce", vectorstore=db)
+
+    # LangChainのAgentとToolsを定義
+    tools = [
+        Tool(
+          name = "my_searcher",
+          func=qa,
+          description="Langchainの説明"
+      )
+    ]
+    agent = initialize_agent(tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
+
+    # PromptTemplateの定義
+    template = """
+    Please answer the questions below.
+    questions:{question}
+    answer：
+    """
+    prompt = PromptTemplate(
+        input_variables=["question"],
+        template=template,
+    )
+
+    question = prompt.format(question=user_message)
+    agent.run(question)
     
+    #print(answer)
+
     return '未作成：LLama2のDB版'
